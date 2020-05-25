41c57bd6a4875cb61f55569fb3e07c851985cca8
Andres Baldarrago <andres.gastulo@gmail.com>
2020-05-25 16:48:11 -0500

    Implementando el login.

---
+++ Server/UserDA.vb
@@ -0,0 +1,14 @@
+﻿Imports Data
+
+Public Class UserDA
+    Private ReadOnly DB As DBAttendanceEntities = New DBAttendanceEntities()
+
+    Public Function Login(name As String, password As String) As User
+        Dim user = From u In DB.User Where u.Name Is name And u.Password Is password Select u
+        Try
+        Return user.Single()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+End Class

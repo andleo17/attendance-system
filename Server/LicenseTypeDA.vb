ef1df43143c6ae95c06e310d5847f96957e23332
PaolaCieza <paolacieza8@gmail.com>
2020-06-03 09:45:56 -0500

    avance de tipo de licencia

---
+++ Server/LicenseTypeDA.vb
@@ -0,0 +1,29 @@
+﻿Imports Data
+Public Class LicenseTypeDA
+    Public Shared Function listarTiposLicencia() As List(Of LicenseType)
+        Using DB = New DBAttendanceEntities
+            Try
+                Dim Lista = DB.LicenseType
+                'Dim lista = From LT In DB.LicenseType Select LT.id 
+                Return Lista.ToList
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Using
+    End Function
+
+
+    Public Shared Function save(LicenseType As LicenseType)
+        Using DB = New DBAttendanceEntities
+            Try
+                DB.LicenseType.Add(LicenseType)
+                DB.SaveChanges()
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Using
+    End Function
+
+
+
+End Class
--- View/FrmTipoLicencia.xaml.vb
+++ View/FrmTipoLicencia.xaml.vb
@@ -1,3 +1,41 @@
-﻿Class FrmTipoLicencia
+﻿Imports Server
+Imports Data
+Class FrmTipoLicencia
 
+    Private Sub listLicenseType()
+        Try
+            ListaTipoLicencia.ItemsSource = LicenseTypeDA.listarTiposLicencia
+
+        Catch ex As Exception
+            MessageBox.Show("Error al listar", "Sistema asistencia", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+    Private Sub Page_Initialized(sender As Object, e As EventArgs)
+        listLicenseType()
+    End Sub
+
+
+    Private Sub saveLicenseType()
+        Try
+            Dim LicenseType = SetLicenseTypeDa(New LicenseType)
+            LicenseTypeDA.save(LicenseType)
+            MessageBox.Show("Tipo de licencia agregada de manera exitosa")
+            listLicenseType()
+        Catch ex As Exception
+            MessageBox.Show("Error al registrar", MessageBoxImage.Error)
+        End Try
+    End Sub
+
+
+    Private Function SetLicenseTypeDa(LicenseType As LicenseType) As LicenseType
+        LicenseType.Id = txtCodigo.Text
+        LicenseType.Description = txtDescripcion.Text
+        LicenseType.MaximumDays = txtDias.Text
+        Return LicenseType
+    End Function
+
+    Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs) Handles btnSave.Click
+        saveLicenseType()
+    End Sub
 End Class

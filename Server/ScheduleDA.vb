801623facb26eb43b36163101060e60918730510
Andres Baldarrago <andres.gastulo@gmail.com>
2020-06-04 00:02:27 -0500

    Implementando agregar empleado.

--- Server/ScheduleDA.vb
+++ Server/ScheduleDA.vb
@@ -2,15 +2,14 @@
 Public Class ScheduleDA
 
 	Public Shared Function FindActual(Employee As Employee) As Schedule
-		Using DB = New DBAttendanceEntities
-			Try
-				Employee = DB.Employee.Find(Employee.CardId)
-				Dim Schedule = From S In Employee.Schedule Where S.State Select S
-				Return Schedule.Single
-			Catch ex As Exception
-				Throw ex
-			End Try
-		End Using
+		Try
+			Dim DB = New DBAttendanceEntities
+			Employee = DB.Employee.Find(Employee.CardId)
+			Dim Schedule = From S In Employee.Schedule Where S.State Select S
+			Return Schedule.Single
+		Catch ex As Exception
+			Throw ex
+		End Try
 	End Function
 
 	Public Shared Sub Save(Schedule As Schedule)
--- View/FrmEmployeeForm.xaml.vb
+++ View/FrmEmployeeForm.xaml.vb
@@ -5,6 +5,8 @@ Class FrmEmployeeForm
     Public Property SelectedEmployee As Employee
     Public Property Mode As Integer
 
+    Private ScheduleDetails As List(Of ScheduleDetail) = New List(Of ScheduleDetail)
+
     Private Sub Back()
         NavigationService.GetNavigationService(Me).GoBack()
     End Sub
@@ -16,7 +18,6 @@ Class FrmEmployeeForm
     End Class
 
     Private Sub GenerateSchedule()
-
         Dim Hours = New List(Of Hour)
         For i As Integer = 7 To 21
             Dim SH = New TimeSpan(i, 0, 0)
@@ -29,6 +30,18 @@ Class FrmEmployeeForm
             Hours.Add(Hour)
         Next
         DgdSchedule.ItemsSource = Hours
+    End Sub
+
+    Private Sub PaintSchedule(ScheduleDetail As ScheduleDetail)
+        Dim HoursInDays = DgdSchedule.Columns.Item(ScheduleDetail.Day)
+        For Each H In DgdSchedule.Items
+            If ScheduleDetail.InHour <= H.StartHour And H.FinishHour <= ScheduleDetail.OutHour Then
+                Dim C As TextBlock = HoursInDays.GetCellContent(H)
+                If C IsNot Nothing Then
+                    C.Background = New SolidColorBrush(Colors.Yellow)
+                End If
+            End If
+        Next
     End Sub
 
     Private Sub DisableFields()
@@ -97,8 +110,23 @@ Class FrmEmployeeForm
         Schedule.EmployeeCardId = TxtCardId.Text
         Schedule.FinishDate = DpkScheduleFinalDate.SelectedDate
         Schedule.StartDate = DpkScheduleInitialDate.SelectedDate
+        Schedule.ScheduleDetail = ScheduleDetails
         Return Schedule
     End Function
+
+    Private Sub GetScheduleDetail()
+        Dim Cells = DgdSchedule.SelectedCells
+        Dim StartHour = Cells.First().Item.StartHour
+        Dim FinishHour = Cells.Last().Item.FinishHour
+        Dim Day = Cells.First().Column.DisplayIndex
+        Dim ScheduleDetail = New ScheduleDetail With {
+            .InHour = StartHour,
+            .OutHour = FinishHour,
+            .Day = Day
+        }
+        ScheduleDetails.Add(ScheduleDetail)
+        PaintSchedule(ScheduleDetail)
+    End Sub
 
     Private Sub ShowSchedule()
         Try
@@ -106,6 +134,9 @@ Class FrmEmployeeForm
             ChkSchedule.IsChecked = Schedule.State
             DpkScheduleInitialDate.SelectedDate = Schedule.StartDate
             DpkScheduleFinalDate.SelectedDate = Schedule.FinishDate
+            For Each SD In Schedule.ScheduleDetail
+                PaintSchedule(SD)
+            Next
         Catch ex As Exception
             MessageBox.Show("Error al encontrar el horario")
         End Try
@@ -116,9 +147,9 @@ Class FrmEmployeeForm
             Dim Employee = SetEmployeeData(New Employee)
             Dim Contract = SetContractData(New Contract)
             Dim Schedule = SetScheduleData(New Schedule)
+            Employee.Contract.Add(Contract)
+            Employee.Schedule.Add(Schedule)
             EmployeeDA.Save(Employee)
-            ContractDA.Save(Contract)
-            ScheduleDA.Save(Schedule)
             MessageBox.Show("Empleado agregado correctamente")
             Back()
         Catch ex As Exception
@@ -144,14 +175,13 @@ Class FrmEmployeeForm
     End Sub
 
     Private Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
-        GenerateSchedule()
         If Mode.Equals(0) Then
             Button.Content = "REGISTRAR"
         ElseIf Mode.Equals(1) Then
             Button.Content = "GUARDAR"
             ShowEmployee()
             ShowContract()
-            ShowContract()
+            ShowSchedule()
         ElseIf Mode.Equals(2) Then
             Button.Content = "VOLVER"
             ShowEmployee()
@@ -166,5 +196,13 @@ Class FrmEmployeeForm
 
     Private Sub DpkContractFinalDate_SelectedDateChanged(sender As Object, e As SelectionChangedEventArgs) Handles DpkContractFinalDate.SelectedDateChanged
         DpkScheduleFinalDate.SelectedDate = DpkContractFinalDate.SelectedDate
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        GetScheduleDetail()
+    End Sub
+
+    Private Sub Page_Initialized(sender As Object, e As EventArgs)
+        GenerateSchedule()
     End Sub
 End Class

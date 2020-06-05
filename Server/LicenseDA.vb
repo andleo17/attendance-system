3b17be04253c5eae05abf95cb43c9babda3d7d98
PaolaCieza <paolacieza8@gmail.com>
2020-06-04 20:58:54 -0500

    licencia avance

--- Server/LicenseDA.vb
+++ Server/LicenseDA.vb
@@ -1,3 +1,78 @@
-﻿Public Class LicenseDA
+﻿Imports Data
+Public Class LicenseDA
+    Public Shared Function List() As List(Of License)
+        Using DB = New DBAttendanceEntities
+            Try
+                Dim Lista = DB.License
+                Return Lista.ToList
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Using
+
+    End Function
+
+
+    Public Shared Function Search(License As License) As License
+        Try
+            Dim DB = New DBAttendanceEntities()
+            Return DB.License.Find(License.Id)
+        Catch ex As Exception
+            Throw ex
+        End Try
+
+    End Function
+
+
+    Public Shared Sub Save(License As License)
+        Using DB = New DBAttendanceEntities()
+            Try
+                DB.License.Add(License)
+                DB.SaveChanges()
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Using
+
+    End Sub
+
+    Public Shared Sub Update(License As License)
+        Using DB = New DBAttendanceEntities()
+            Try
+                Dim oldLicense = DB.License.Find(License.Id)
+                DB.Entry(oldLicense).CurrentValues.SetValues(License)
+                DB.SaveChanges()
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Using
+    End Sub
+
+
+    Public Shared Sub Delete(License As License)
+        Using DB = New DBAttendanceEntities()
+            Try
+                License = DB.License.Find(License.Id)
+                DB.License.Remove(License)
+                DB.SaveChanges()
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Using
+    End Sub
+
+
+    'Public Shared Function ListLicenseType() As List(Of LicenseType)
+    '    Try
+    '        Dim DB = New DBAttendanceEntities()
+    '        Dim Lista = DB.License
+    '        Return
+    '    Catch ex As Exception
+    '        Throw ex
+    '    End Try
+
+    'End Function
+
+
 
 End Class
--- View/FrmTipoLicencia.xaml.vb
+++ View/FrmTipoLicencia.xaml.vb
@@ -22,6 +22,7 @@ Class FrmTipoLicencia
             LicenseTypeDA.save(LicenseType)
             MessageBox.Show("Tipo de licencia agregada de manera exitosa")
             listLicenseType()
+            ClearInputs()
         Catch ex As Exception
             MessageBox.Show("Error al registrar", MessageBoxImage.Error)
         End Try
@@ -56,10 +57,53 @@ Class FrmTipoLicencia
             LicenseTypeDA.Update(SelectedLicenseType)
             MessageBox.Show("Tipo de licencia actualizado")
             ShowLicenseType()
-
+            ClearInputs()
         End If
         MessageBox.Show("Seleccione tipo de licencia")
     End Function
+
+
+    Private Function DeleteLicenseType()
+        If SelectedLicenseType IsNot Nothing Then
+            Dim Msg, Style, Title, Response
+            Msg = "¿Seguro que desea eliminar el tipo de licencia"
+            Style = vbYesNo + vbCritical + vbDefaultButton2
+            Title = ".:SISTEMA DE ASISTENCIA:."
+            Response = MsgBox(Msg, Style, Title)
+            If Response = vbYes Then
+                SelectedLicenseType = SetLicenseTypeDa(SelectedLicenseType)
+                LicenseTypeDA.Delete(SelectedLicenseType)
+                MessageBox.Show("Tipo de licencia eliminada")
+                listLicenseType()
+                ClearInputs()
+            End If
+        Else
+            MessageBox.Show("Seleccione un tipo de licencia")
+        End If
+    End Function
+
+    Private Sub ClearInputs()
+        SelectedLicenseType = Nothing
+        txtCodigo.Text = Nothing
+        txtDescripcion.Text = Nothing
+        txtDias.Text = Nothing
+    End Sub
+
+    Private Sub SearchLicenseType(LicenseType As LicenseType)
+        Try
+            LicenseType.Id = txtCodigo.Text
+            Dim LT = LicenseTypeDA.Search(LicenseType)
+            If LT IsNot Nothing Then
+                LicenseType = LT
+                SelectedLicenseType = LicenseType
+                ShowLicenseType()
+                txtCodigo.IsEnabled = False
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
 
     Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs) Handles btnSave.Click
         saveLicenseType()
@@ -72,4 +116,25 @@ Class FrmTipoLicencia
     Private Sub btnUpdate_Click(sender As Object, e As RoutedEventArgs) Handles btnUpdate.Click
         UpdateLicenseType()
     End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As RoutedEventArgs) Handles btnDelete.Click
+        DeleteLicenseType()
+    End Sub
+
+    Private Sub btnClean_Click(sender As Object, e As RoutedEventArgs) Handles btnClean.Click
+        ClearInputs()
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As RoutedEventArgs) Handles btnSearch.Click
+        If txtCodigo.Text.Length <> 0 Then
+            SearchLicenseType(New LicenseType)
+        Else
+            MessageBox.Show("Ingrese código de tipo de licensia")
+        End If
+    End Sub
+
+
+
+
+
 End Class

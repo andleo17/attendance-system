fba12ce0aecc40ca2c9c2f7223e9e587a0899ecc
jesuslujan98 <51263684+jesuslujan98@users.noreply.github.com>
2020-06-04 21:03:41 -0500

    Avance de reportes

--- View/FrmConsultaAsistenciaMes.xaml.vb
+++ View/FrmConsultaAsistenciaMes.xaml.vb
@@ -1,5 +1,15 @@
-﻿Class FrmConsultaAsistenciaMes
+﻿Imports Stimulsoft
+Imports Stimulsoft.Report
+
+Class FrmConsultaAsistenciaMes
     Private Sub DatePicker_CalendarClosed(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim Report = New StiReport()
+        Report.Load("C://Users//USUARIO//Documents//GitHub//attendance-system//View//reports//licence_report.mrt")
+        Report.Show()
 
     End Sub
 End Class

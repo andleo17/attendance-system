777e06ce00fb3af9a074becb271bbb571b85e203
PaolaCieza <paolacieza8@gmail.com>
2020-06-04 21:13:30 -0500

    boton

--- View/FrmConsultaAsistenciaMes.xaml.vb
+++ View/FrmConsultaAsistenciaMes.xaml.vb
@@ -2,4 +2,8 @@
     Private Sub DatePicker_CalendarClosed(sender As Object, e As RoutedEventArgs)
 
     End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
 End Class

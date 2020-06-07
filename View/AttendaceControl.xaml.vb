19358a6588ab046b6077e0ed0d64d0caf90387d1
Andres Baldarrago <andres.gastulo@gmail.com>
2020-06-07 17:32:01 -0500

    Agregando el control de asistencia.

--- View/AttendaceControl.xaml.vb
+++ View/AttendaceControl.xaml.vb
@@ -1,3 +1,58 @@
-﻿Class AttendaceControl
+﻿Imports Data
 
+Class AttendaceControl
+	Dim AttendanceToday As Attendance
+	Dim SD As ScheduleDetail
+
+	Private Function Format(Hour As TimeSpan) As String
+		Dim D = Today.Add(Hour)
+		Return D.ToString("T")
+	End Function
+
+	Private Sub UserControl_Loaded(sender As Object, e As RoutedEventArgs)
+		SD = DataContext
+		TxtInHour.Content = Format(SD.InHour)
+		TxtOutHour.Content = Format(SD.OutHour)
+		Try
+			AttendanceToday = SD.Schedule.Employee.Attendance.First(Function(A) A.Date = Today)
+		Catch ex As Exception
+			AttendanceToday = Nothing
+		End Try
+		If AttendanceToday Is Nothing Then
+			TxtInHour.Foreground = New SolidColorBrush(Colors.Gray)
+			TxtOutHour.Foreground = New SolidColorBrush(Colors.Gray)
+		Else
+			If AttendanceToday.OutHour Is Nothing Then
+				TxtOutHour.Foreground = New SolidColorBrush(Colors.Gray)
+			End If
+		End If
+	End Sub
+
+	Private Sub BtnShow_Click(sender As Object, e As RoutedEventArgs) Handles BtnShow.Click
+		'Agregar asistencia
+		' Comparar si llegó tarde
+		Dim Ahorita = Now.TimeOfDay
+		Dim Tolerance = TimeSpan.FromMinutes(10)
+		If Ahorita >= SD.InHour.Subtract(Tolerance) Then
+			TxtInHour.Content = Format(Ahorita)
+			If Ahorita <= SD.InHour.Add(Tolerance) Then
+				TxtInHour.Foreground = New SolidColorBrush(Colors.White)
+			Else
+				TxtInHour.Foreground = New SolidColorBrush(Colors.Crimson)
+			End If
+		End If
+	End Sub
+
+	Private Sub BtnModify_Click(sender As Object, e As RoutedEventArgs) Handles BtnModify.Click
+		'Actualizar asistencia
+		Dim Ahorita = Now.TimeOfDay
+		Dim Tolerance = TimeSpan.FromMinutes(10)
+		TxtOutHour.Foreground = New SolidColorBrush(Colors.White)
+		If Ahorita >= SD.OutHour.Add(Tolerance) Then
+			TxtOutHour.FontFamily = New FontFamily("Segoe UI Semibold")
+		ElseIf Ahorita <= SD.OutHour.Subtract(Tolerance) Then
+			TxtInHour.Foreground = New SolidColorBrush(Colors.Crimson)
+		End If
+		TxtOutHour.Content = Format(Ahorita)
+	End Sub
 End Class
--- View/FrmAttendaceList.xaml.vb
+++ View/FrmAttendaceList.xaml.vb
@@ -1,3 +1,28 @@
-﻿Class FrmAttendaceList
+﻿Imports Data
+Imports Server
 
+Class FrmAttendaceList
+	Private Sub FilterEmployeeList()
+		CollectionViewSource.GetDefaultView(EmployeeList.ItemsSource).Refresh()
+	End Sub
+
+	Private Sub BtnAdd_Click(sender As Object, e As RoutedEventArgs) Handles BtnAdd.Click
+		NavigationService.GetNavigationService(Me).Content = New FrmLicencia
+	End Sub
+
+	Private Sub TxtFilter_TextChanged(sender As Object, e As RoutedEventArgs) Handles TxtFilter.TextChanged
+		FilterEmployeeList()
+	End Sub
+
+	Private Sub Page_Initialized(sender As Object, e As EventArgs)
+		Dim EList = (From S In AttendanceDA.List(Today)
+					 Order By S.Schedule.Employee.Lastname Descending, S.Schedule.Employee.Name Descending
+					 Select S).ToList
+		EmployeeList.ItemsSource = EList
+		Dim v = CollectionViewSource.GetDefaultView(EList)
+		v.Filter = Function(S As ScheduleDetail)
+					   Return S.Schedule.Employee.Name.ToLower.Contains(TxtFilter.Text.ToLower) Or
+					   S.Schedule.Employee.Lastname.ToLower.Contains(TxtFilter.Text.ToLower)
+				   End Function
+	End Sub
 End Class
